7d212a2eb05b8dd4dcf659c0c560d761fdf3dbd7
Travis Gross <gross.t@choiceenergyservices.com>
2022-10-19 14:29:09 -0500

    commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom/client'
+import App from './components/App';
+
+const el = (document.getElementById('root'))
+const root = ReactDom.createRoot(el!);
+
+root.render(<App />)
---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,34 @@
+import { ActionType } from '../action-types'
+import { Action } from '../actions'
+
+interface IRepositoriesState {
+     loading: boolean
+     error: string | null
+     data: string[]
+}
+const intitialState = {
+     loading: false,
+     error: null,
+     data: []
+}
+const repositoriesReducer = (
+     state: IRepositoriesState = intitialState,
+     action: Action
+): IRepositoriesState => {
+     switch (action.type) {
+          case ActionType.SEARCH_REPOSITORIES:
+               return { loading: true, error: null, data: [] }
+
+          case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+               return { loading: false, error: null, data: action.payload }
+
+          case ActionType.SEARCH_REPOSITORIES_ERROR:
+               return { loading: false, error: action.payload, data: [] }
+
+          default:
+               return state;
+     }
+
+}
+
+export default repositoriesReducer
